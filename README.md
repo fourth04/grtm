5f869b11ebc7d28de6d021c427948868db6b08c4
gdgyy <Gyangyun@gmail.com>
2018-03-18 13:10:01 +0800

    重新修改

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# about grtm 
+# about grtm
 [![Build Status](https://travis-ci.org/scottkiss/grtm.svg?branch=master)](https://travis-ci.org/scottkiss/grtm)
 
 grtm is a tool to manage golang goroutines.use this can start or stop a long loop goroutine.
 
 ## Getting started
 ```bash
-go get github.com/scottkiss/grtm
+go get github.com/fourth04/grtm
 ```
 
 ## Create normal goroutine
@@ -15,7 +15,7 @@ package main
 
 import (
         "fmt"
-        "github.com/scottkiss/grtm"
+        "github.com/fourth04/grtm"
         "time"
        )
 
@@ -24,12 +24,11 @@ func normal() {
 }
 
 func main() {
-        gm := grtm.NewGrManager()
-        gm.NewGoroutine("normal", normal)
+        gm := &grtm.GrManager{}
+        gm.NewNormalGoroutine("normal", normal)
         fmt.Println("main function")
         time.Sleep(time.Second * time.Duration(5))
 }
-~
 ```
 
 ## Create normal goroutine function with params
@@ -38,26 +37,27 @@ func main() {
 package main
 
 import (
-        "fmt"
-        "github.com/scottkiss/grtm"
-        "time"
-       )
+	"fmt"
+	"time"
+
+	"github.com/fourth04/grtm"
+)
 
 func normal() {
-    fmt.Println("i am normal goroutine")
+	fmt.Println("i am normal goroutine")
 }
 
 func funcWithParams(args ...interface{}) {
-    fmt.Println(args[0].([]interface{})[0].(string))
-    fmt.Println(args[0].([]interface{})[1].(string))
+	fmt.Println(args[0].([]interface{})[0].(string))
+	fmt.Println(args[0].([]interface{})[1].(string))
 }
 
 func main() {
-        gm := grtm.NewGrManager()
-        gm.NewGoroutine("normal", normal)
-        fmt.Println("main function")
-        gm.NewGoroutine("funcWithParams", funcWithParams, "hello", "world")
-        time.Sleep(time.Second * time.Duration(5))
+	gm := &grtm.GrManager{}
+	gm.NewNormalGoroutine("normal", normal)
+	fmt.Println("main function")
+	gm.NewNormalGoroutine("funcWithParams", funcWithParams, "hello", "world")
+	time.Sleep(time.Second * time.Duration(5))
 }
 ```
 
@@ -68,23 +68,23 @@ package main
 
 import (
         "fmt"
-        "github.com/scottkiss/grtm"
+        "github.com/fourth04/grtm"
         "time"
        )
 
 func myfunc() {
-    fmt.Println("do something repeat by interval 4 seconds")
-        time.Sleep(time.Second * time.Duration(4))
+	fmt.Println("do something repeat by interval 4 seconds")
+	time.Sleep(time.Second * time.Duration(4))
 }
 
 func main() {
-gm := grtm.NewGrManager()
-        gm.NewLoopGoroutine("myfunc", myfunc)
-        fmt.Println("main function")
-        time.Sleep(time.Second * time.Duration(40))
-        fmt.Println("stop myfunc goroutine")
-        gm.StopLoopGoroutine("myfunc")
-        time.Sleep(time.Second * time.Duration(80))
+	gm := &grtm.GrManager{}
+	gm.NewLoopGoroutine("myfunc", myfunc)
+	fmt.Println("main function")
+	time.Sleep(time.Second * time.Duration(20))
+	fmt.Println("stop myfunc goroutine")
+	gm.StopGoroutine("myfunc")
+	time.Sleep(time.Second * time.Duration(30))
 }
 ```
 
@@ -102,19 +102,77 @@ no signal
 do something repeat by interval 4 seconds
 no signal
 do something repeat by interval 4 seconds
-no signal
-do something repeat by interval 4 seconds
-no signal
-do something repeat by interval 4 seconds
-no signal
-do something repeat by interval 4 seconds
-no signal
-do something repeat by interval 4 seconds
-no signal
-do something repeat by interval 4 seconds
 stop myfunc goroutine
-gid[5577006791947779410] quit
-
+[gid: 5577006791947779410, name: myfunc] quit
 ```
 
+## Create diy goroutine
 
+```golang
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/fourth04/grtm"
+)
+
+func diy(chMsg chan string) {
+	for {
+		select {
+		case <-chMsg:
+			return
+		default:
+			fmt.Println("no signal")
+		}
+		fmt.Println("i am diy goroutine")
+		time.Sleep(time.Second * time.Duration(1))
+	}
+}
+
+func main() {
+	gm := &grtm.GrManager{}
+	gm.NewDiyGoroutine("diy", diy)
+	fmt.Println("main function")
+	time.Sleep(time.Second * time.Duration(5))
+    gm.StopDiyGoroutine("diy")
+	time.Sleep(time.Second * time.Duration(10))
+}
+```
+
+## Create diy goroutine function with params
+
+```go
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/fourth04/grtm"
+)
+
+func funcWithParams(chMsg chan string, args ...interface{}) {
+	for {
+		select {
+		case <-chMsg:
+			return
+		default:
+            fmt.Println("no signal")
+            fmt.Println(args[0].([]interface{})[0].(string))
+            fmt.Println(args[0].([]interface{})[1].(string))
+            time.Sleep(time.Second * time.Duration(1))
+        }
+	}
+}
+
+func main() {
+	gm := &grtm.GrManager{}
+	gm.NewDiyGoroutine("funcWithParams", funcWithParams, "hello", "world")
+	fmt.Println("main function")
+	time.Sleep(time.Second * time.Duration(5))
+	gm.StopDiyGoroutine("funcWithParams")
+	time.Sleep(time.Second * time.Duration(10))
+}
+```
--- grmanager.go
+++ grmanager.go
@@ -2,68 +2,62 @@ package grtm
 
 import (
 	"fmt"
+	"math/rand"
 	"strconv"
 	"strings"
+	"sync"
 )
 
+const (
+	// STOP signal
+	STOP = "__P"
+)
+
+// GoroutineChannel associate name and channel
+type GoroutineChannel struct {
+	gid   uint64
+	name  string
+	chMsg chan string
+}
+
+// GrManager create a locked map[string]*GoroutineChannel
 type GrManager struct {
-	grchannelMap *GoroutineChannelMap
+	mutex      sync.Mutex
+	grchannels map[string]*GoroutineChannel
 }
 
-func NewGrManager() *GrManager {
-	gm := &GoroutineChannelMap{}
-	return &GrManager{grchannelMap: gm}
-}
-
-func (gm *GrManager) StopLoopGoroutine(name string) error {
-	stopChannel, ok := gm.grchannelMap.grchannels[name]
-	if !ok {
-		return fmt.Errorf("not found goroutine name :" + name)
+func (gm *GrManager) register(name string) error {
+	gchannel := &GoroutineChannel{
+		gid:  uint64(rand.Int63()),
+		name: name,
 	}
-	gm.grchannelMap.grchannels[name].msg <- STOP + strconv.Itoa(int(stopChannel.gid))
+	gchannel.chMsg = make(chan string)
+	gm.mutex.Lock()
+	defer gm.mutex.Unlock()
+	if gm.grchannels == nil {
+		gm.grchannels = make(map[string]*GoroutineChannel)
+	} else if _, ok := gm.grchannels[gchannel.name]; ok {
+		return fmt.Errorf("goroutine channel already defined: %q", gchannel.name)
+	}
+	gm.grchannels[gchannel.name] = gchannel
 	return nil
 }
 
-func (gm *GrManager) NewLoopGoroutine(name string, fc interface{}, args ...interface{}) {
-	go func(this *GrManager, n string, fc interface{}, args ...interface{}) {
-		//register channel
-		err := this.grchannelMap.register(n)
-		if err != nil {
-			return
-		}
-		for {
-			select {
-			case info := <-this.grchannelMap.grchannels[name].msg:
-				taskInfo := strings.Split(info, ":")
-				signal, gid := taskInfo[0], taskInfo[1]
-				if gid == strconv.Itoa(int(this.grchannelMap.grchannels[name].gid)) {
-					if signal == "__P" {
-						fmt.Println("gid[" + gid + "] quit")
-						this.grchannelMap.unregister(name)
-						return
-					} else {
-						fmt.Println("unknown signal")
-					}
-				}
-			default:
-				fmt.Println("no signal")
-			}
-
-			if len(args) > 1 {
-				fc.(func(...interface{}))(args)
-			} else if len(args) == 1 {
-				fc.(func(interface{}))(args[0])
-			} else {
-				fc.(func())()
-			}
-		}
-	}(gm, name, fc, args...)
+func (gm *GrManager) unregister(name string) error {
+	gm.mutex.Lock()
+	defer gm.mutex.Unlock()
+	if _, ok := gm.grchannels[name]; !ok {
+		return fmt.Errorf("goroutine channel not find: %q", name)
+	}
+	delete(gm.grchannels, name)
+	return nil
 }
 
-func (gm *GrManager) NewGoroutine(name string, fc interface{}, args ...interface{}) {
-	go func(n string, fc interface{}, args ...interface{}) {
+// NewNormalGoroutine create a normal goroutine including register and unregister, but the register and unregister make no sense, the goroutine will stop itself
+func (gm *GrManager) NewNormalGoroutine(name string, fc interface{}, args ...interface{}) {
+	go func() {
 		//register channel
-		err := gm.grchannelMap.register(n)
+		err := gm.register(name)
 		if err != nil {
 			return
 		}
@@ -74,7 +68,82 @@ func (gm *GrManager) NewGoroutine(name string, fc interface{}, args ...interface
 		} else {
 			fc.(func())()
 		}
-		gm.grchannelMap.unregister(name)
-	}(name, fc, args...)
+		gm.unregister(name)
+	}()
+}
 
+// NewLoopGoroutine create a loop goroutine including register, it will repeatitively execute the function until a STOP message is sended in the message channel
+func (gm *GrManager) NewLoopGoroutine(name string, fc interface{}, args ...interface{}) {
+	go func() {
+		//register channel
+		err := gm.register(name)
+		if err != nil {
+			return
+		}
+		for {
+			select {
+			case info := <-gm.grchannels[name].chMsg:
+				taskInfo := strings.Split(info, ":")
+				signal, gid := taskInfo[0], taskInfo[1]
+				if gid == strconv.Itoa(int(gm.grchannels[name].gid)) {
+					if signal == STOP {
+						fmt.Printf("[gid: %s, name: %s] quit\n", gid, name)
+						gm.unregister(name)
+						return
+					}
+					fmt.Println("unknown signal")
+				}
+			default:
+				fmt.Println("no signal")
+				if len(args) > 1 {
+					fc.(func(...interface{}))(args)
+				} else if len(args) == 1 {
+					fc.(func(interface{}))(args[0])
+				} else {
+					fc.(func())()
+				}
+			}
+		}
+	}()
+}
+
+// StopGoroutine sends a STOP message to the named message channel to close the associated goroutine
+func (gm *GrManager) StopGoroutine(name string) error {
+	stopChannel, ok := gm.grchannels[name]
+	if !ok {
+		return fmt.Errorf("not found goroutine name :" + name)
+	}
+	gid := strconv.Itoa(int(stopChannel.gid))
+	gm.grchannels[name].chMsg <- STOP + ":" + gid
+	return nil
+}
+
+// NewDiyGoroutine create a diy goroutine, the "diy" means the function will define a chMsg parameter which like a slot, NewDiyGoroutine will register a channel and send it to the function, so the function will use the chMsg to stop
+func (gm *GrManager) NewDiyGoroutine(name string, fc interface{}, args ...interface{}) {
+	go func() {
+		//register channel
+		err := gm.register(name)
+		if err != nil {
+			return
+		}
+		if len(args) >= 1 {
+			fc.(func(chan string, ...interface{}))(gm.grchannels[name].chMsg, args)
+		} else if len(args) == 1 {
+			fc.(func(chan string, interface{}))(gm.grchannels[name].chMsg, args[0])
+		} else {
+			fc.(func(chan string))(gm.grchannels[name].chMsg)
+		}
+	}()
+}
+
+// StopDiyGoroutine sends a STOP message to the named message channel to close the associated goroutine, prints quit infomation and unregister the name
+func (gm *GrManager) StopDiyGoroutine(name string) error {
+	err := gm.StopGoroutine(name)
+	if err != nil {
+		return err
+	}
+	gid := strconv.Itoa(int(gm.grchannels[name].gid))
+	fmt.Printf("[gid: %s, name: %s] quit\n", gid, name)
+	gm.unregister(name)
+	return nil
 }
